567f4581ea99aa3acdc17577cd174ec9315963d8
Rodrigo <reyes.rr@gmail.com>
2014-03-23 13:05:20 +0100

    action post-publication

--- backend/src/main/webapp/js/home.js
+++ backend/src/main/webapp/js/home.js
@@ -1,7 +1,11 @@
 (function(Readily){
 
-	Readily.Navigation.mkRoute("", "#home", "home_tmpl.html", function(domId, template) {
-		$(domId).html(template());
-	});
+	function home(domId, template) {
+		$(domId).html(template());		
+	}
+	
+	Readily.Navigation.mkRoute("", "#home", "home_tmpl.html", home);
+	Readily.Navigation.mkRoute("/home", "#home", "home_tmpl.html", home);
+	Readily.Navigation.mkRoute("home", "#home", "home_tmpl.html", home);
 
 })(window.Readily);
